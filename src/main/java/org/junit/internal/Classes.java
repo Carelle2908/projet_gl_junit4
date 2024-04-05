3b7c7c3e4fb2fd8fd4614d6546b220786eb41539
Hakizimana <boris.hakizimana.etu@univ-lille.fr>
2024-04-05 17:08:01 +0200

    reduction de la complexité cyclomatique d'une classe

--- src/main/java/org/junit/internal/Classes.java
+++ src/main/java/org/junit/internal/Classes.java
@@ -9,34 +9,54 @@ public class Classes {
 
     /**
      * Do not instantiate.
+     * 
      * @deprecated will be private soon.
      */
-    @Deprecated
-    public Classes() {
+    //@Deprecated
+
+    // Ce constructeur est privé pour empêcher l'instanciation de la classe.
+    private Classes() {
     }
 
     /**
-     * Returns Class.forName for {@code className} using the current thread's class loader.
-     * If the current thread does not have a class loader, falls back to the class loader for
-     * {@link Classes}.
+     * Returns Class.forName for {@code className} using the current thread's
+     * class loader. If the current thread does not have a class loader, falls
+     * back to the class loader for {@link Classes}.
      *
      * @param className Name of the class.
+     * @throws ClassNotFoundException if the class cannot be found.
      */
     public static Class<?> getClass(String className) throws ClassNotFoundException {
         return getClass(className, Classes.class);
     }
 
     /**
-     * Returns Class.forName for {@code className} using the current thread's class loader.
-     * If the current thread does not have a class loader, falls back to the class loader for the
-     * passed-in class.
+     * Returns Class.forName for {@code className} using the current thread's
+     * class loader. If the current thread does not have a class loader, falls
+     * back to the class loader for the passed-in class.
      *
-     * @param className Name of the class.
-     * @param callingClass Class that is requesting a the class
+     * @param className    Name of the class.
+     * @param callingClass Class that is requesting the class.
+     * @throws ClassNotFoundException if the class cannot be found.
      * @since 4.13
      */
     public static Class<?> getClass(String className, Class<?> callingClass) throws ClassNotFoundException {
         ClassLoader classLoader = currentThread().getContextClassLoader();
+        return loadClass(className, callingClass, classLoader);
+    }
+
+    /**
+     * Loads the class using the provided class loader. Falls back to the class
+     * loader for the passed-in class if the current thread does not have a
+     * class loader.
+     *
+     * @param className    Name of the class.
+     * @param callingClass Class that is requesting the class.
+     * @param classLoader  The class loader to use.
+     * @throws ClassNotFoundException if the class cannot be found.
+     */
+    private static Class<?> loadClass(String className, Class<?> callingClass, ClassLoader classLoader)
+            throws ClassNotFoundException {
         return Class.forName(className, true, classLoader == null ? callingClass.getClassLoader() : classLoader);
     }
 }
