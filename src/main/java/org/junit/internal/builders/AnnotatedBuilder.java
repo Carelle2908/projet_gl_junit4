a3b4884be75e33157c8a379a6cd153596a2c74a8
Hakizimana <boris.hakizimana.etu@univ-lille.fr>
2024-04-05 16:03:52 +0200

    reorganisation de quelques classes

--- src/main/java/org/junit/internal/builders/AnnotatedBuilder.java
+++ src/main/java/org/junit/internal/builders/AnnotatedBuilder.java
@@ -90,27 +90,29 @@ public class AnnotatedBuilder extends RunnerBuilder {
         return null;
     }
 
+    public Runner buildRunner(Class<? extends Runner> runnerClass,
+    Class<?> testClass) throws Exception {
+        try{
+            return runnerClass.getConstructor(Class.class).newInstance(testClass);
+        }
+
+        catch (NoSuchMethodException e) {
+             try {
+                return runnerClass.getConstructor(Class.class,
+                RunnerBuilder.class).newInstance(testClass, suiteBuilder);
+    } catch (NoSuchMethodException e2) {
+        String simpleName = runnerClass.getSimpleName();
+        throw new InitializationError(String.format(
+                CONSTRUCTOR_ERROR_FORMAT, simpleName, simpleName));
+    }
+}
+    }
+
     private Class<?> getEnclosingClassForNonStaticMemberClass(Class<?> currentTestClass) {
         if (currentTestClass.isMemberClass() && !Modifier.isStatic(currentTestClass.getModifiers())) {
             return currentTestClass.getEnclosingClass();
         } else {
             return null;
-        }
-    }
-
-    public Runner buildRunner(Class<? extends Runner> runnerClass,
-            Class<?> testClass) throws Exception {
-        try {
-            return runnerClass.getConstructor(Class.class).newInstance(testClass);
-        } catch (NoSuchMethodException e) {
-            try {
-                return runnerClass.getConstructor(Class.class,
-                        RunnerBuilder.class).newInstance(testClass, suiteBuilder);
-            } catch (NoSuchMethodException e2) {
-                String simpleName = runnerClass.getSimpleName();
-                throw new InitializationError(String.format(
-                        CONSTRUCTOR_ERROR_FORMAT, simpleName, simpleName));
-            }
         }
     }
 }
