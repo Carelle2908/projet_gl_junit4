45fea78d8e0cef6e5d7a08e9c859ea8a411b3ce4
annie.ingabire <boris.hakizimana.etu@univ-lille.fr>
2024-04-06 09:24:14 +0200

    Supprimer de la duplication de codes entre méthodes

--- src/main/java/org/junit/internal/Throwables.java
+++ src/main/java/org/junit/internal/Throwables.java
@@ -154,10 +154,8 @@ public final class Throwables {
     }
 
     private static String getFullStackTrace(Throwable exception) {
-        StringWriter stringWriter = new StringWriter();
-        PrintWriter writer = new PrintWriter(stringWriter);
-        exception.printStackTrace(writer);
-        return stringWriter.toString();
+        
+        return this.getStackTrace(exception);
     }
 
     private static void appendStackTraceLines(
